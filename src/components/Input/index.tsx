66269b99b62f02f6e129dad61c15ab425cfe28e8
Tassio Rego <tassiorego@gmail.com>
2021-03-10 01:52:56 -0300

    feat: adiciona o component de input

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { InputHTMLAttributes } from 'react';
+import { FiSearch } from 'react-icons/fi';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  register?: any;
+}
+
+const Input: React.FC<InputProps> = ({ name, register, ...rest }) => {
+  return (
+    <div className="relative">
+      <span className="absolute left-3 top-1/4 text-lg text-gray-400 ">
+        <FiSearch />
+      </span>
+      <input
+        id={name}
+        name={name}
+        ref={register}
+        className=" bg-gray-800 focus:outline-none w-full text-sm text-black placeholder-gray-400 border border-gray-900 rounded-lg py-2 pl-10 pr-10 h-8"
+        {...rest}
+      />
+    </div>
+  );
+};
+
+export default Input;
