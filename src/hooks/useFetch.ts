3dd907d739bc3e3fcb4a1fc3093bf63256a5826b
Tassio Rego <tassiorego@gmail.com>
2021-03-10 02:04:52 -0300

    feat: criar hook para realizar requisições usando SWR

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import useSWR from 'swr';
+import { AxiosRequestConfig, AxiosError } from 'axios';
+import api from '../services/api';
+
+export default function useFetch<T = any>(
+  url: string,
+  options: AxiosRequestConfig = {},
+) {
+  const { data, error, mutate } = useSWR<T, AxiosError>(url, async uri => {
+    const response = await api.get(uri, options);
+
+    return response.data;
+  });
+
+  return { data, error, isLoading: !data && !error, mutate };
+}
