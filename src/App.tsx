99d5dc044de2f69e0dcfc047d84eb577dcb8bd27
Tassio Rego <tassiorego@gmail.com>
2021-03-10 00:49:39 -0300

    chore: iniciando project com create-react-app e tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Home from './pages/Home';
+import './styles/index.css';
+
+const src: React.FC = () => {
+  return <Home />;
+};
+
+export default src;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Cinema APP</h1>
+    </div>
+  );
+};
+
+export default Home;
