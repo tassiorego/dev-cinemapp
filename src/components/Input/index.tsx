e4c3a50d97bfa6e5aa701a3dc9d181c65747cd08
Tassio Rego <tassiorego@gmail.com>
2021-03-10 02:04:00 -0300

    fix: ajustar input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -10,14 +10,14 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 const Input: React.FC<InputProps> = ({ name, register, ...rest }) => {
   return (
     <div className="relative">
-      <span className="absolute left-3 top-1/4 text-lg text-gray-400 ">
+      <span className="absolute left-2 top-1/4 text-lg text-gray-400 ">
         <FiSearch />
       </span>
       <input
         id={name}
         name={name}
         ref={register}
-        className=" bg-gray-800 focus:outline-none w-full text-sm text-black placeholder-gray-400 border border-gray-900 rounded-lg py-2 pl-10 pr-10 h-8"
+        className=" bg-gray-800 focus:outline-none w-full text-sm text-white placeholder-gray-400 border border-gray-900 rounded-lg py-2 pl-8 h-8"
         {...rest}
       />
     </div>
