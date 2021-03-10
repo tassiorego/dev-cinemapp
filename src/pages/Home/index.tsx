6b29d02d56de1883241f67ec1b9d1f9d8e1fae05
Tassio Rego <tassiorego@gmail.com>
2021-03-10 01:05:20 -0300

    feat: adiciona Header

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
-// import { Container } from './styles';
-
 const Home: React.FC = () => {
   return (
-    <div>
-      <h1>Cinema APP</h1>
-    </div>
+    <>
+      <header className="flex items-center justify-center w-full h-16 bg-gradient-to-b from-red-500 to-red-700">
+        <h1 className="text-white uppercase text-xl font-extrabold italic">
+          Cinema APP
+        </h1>
+      </header>
+    </>
   );
 };
 
